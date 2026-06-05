8d97ffea00d38e57dec4a6076ea8474975e4904c
Ashish <rajputranasingh123495@gmail.com>
2026-06-06 02:53:39 +0530

    project_3 is done

--- project_3.py
+++ project_3.py
@@ -1,11 +1,11 @@
 #Temprature conversion
-select=int(input("Select 1 OR 2 To Enter\n1 for Calcius|\n2 for Fahrenheit|\n:----------"))
+select=int(input("Select 1 OR 2 To Enter\n1 to convert in Fahrenheit|\n2 to convert in Celcius|\n:----------"))
 Temprature=int(input("Enter the Temprature to convert:----"))
 if select==1:
     print("Converting to Fahrenheit.......")
-    print(f"In Fahrenheit:- {(Temprature*(9/5))+32}F")
+    print(f"In Fahrenheit:- {round((Temprature*(9/5))+32,1)}°F")
 elif select==2:
     print("Converting to Celcius.......")
-    print(f"In Celcius:- {((5*Temprature)-160)/9}")
+    print(f"In Celcius:- {round(((5*Temprature)-160)/9,1)}°C")
 else:
     print("Invalid input")
